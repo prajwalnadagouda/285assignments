9cf175ca5458276c80f9c65f9a4380ec6980a660
Prajwal Nadagouda <32608138+prajwalnadagouda@users.noreply.github.com>
2023-10-16 17:15:33 -0700

    changes

--- app.py
+++ app.py
@@ -1,6 +1,39 @@
-from flask import Flask
+from flask import Flask, request
+import yfinance as yf
+from datetime import datetime, timezone
+from requests.exceptions import ConnectionError
+import pytz
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    StockSymbol = request.args.get('stock') 
+    try:
+        # StockSymbol =input("Please enter a symbol:\n")
+        stock = yf.Ticker(StockSymbol)
+        info = stock.info
+        companyName = info['longName']
+        stockPrice = float(info['currentPrice'])
+        previousPrice= float(info['previousClose'])
+        # print(stock_price,previous_day)
+        priceChange= stockPrice-previousPrice
+        percentChange=priceChange/stockPrice*100
+
+    
+        dateandtime = datetime.now(pytz.timezone('America/Los_Angeles')).strftime("%a %b %d %H:%M:%S %Z %Y")
+        message=""
+        message+=dateandtime+"\n"
+        message+=companyName,"("+StockSymbol+")\n"
+        message+=stockPrice, "+" if priceChange >= 0 else "-"+str(abs(round(priceChange,2))), "(+" if percentChange >= 0 else "(-" +str(abs(round(percentChange,2)))+"%)")
+        return(message)
+    except EOFError as e:
+        return("End Reached.")
+    except ConnectionError:
+        return("Network not available. Check your connection.")
+    except Exception as e:
+        return("Details Unavailable. Check your input\n")
+        # break
+
+
     return 'Hello, World!'
